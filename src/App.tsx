c8cb4655f5b0259b09306ee52463fb0a7d586510
BISNISCREATIVE <bisniscreative4.0@gmail.com>
2025-06-14 04:51:38 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,47 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
-import Favorites from "./pages/Favorites";
-import MovieDetail from "./pages/MovieDetail";
+import { FavoritesProvider } from "@/contexts/FavoritesContext";
+import { MovieNavigation } from "@/components/MovieNavigation";
+import { Home } from "@/pages/Home";
+import { Favorites } from "@/pages/Favorites";
+import { MovieDetails } from "@/pages/MovieDetails";
+import { Search } from "@/pages/Search";
 import NotFound from "./pages/NotFound";
+import { useState } from "react";
 
 const queryClient = new QueryClient();
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/favorites" element={<Favorites />} />
-          <Route path="/movie/:id" element={<MovieDetail />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
+const App = () => {
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <FavoritesProvider>
+          <Toaster />
+          <Sonner />
+          <BrowserRouter>
+            <div className="min-h-screen bg-black">
+              <MovieNavigation 
+                onMenuToggle={() => setShowMobileMenu(!showMobileMenu)}
+                showMobileMenu={showMobileMenu}
+              />
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/favorites" element={<Favorites />} />
+                <Route path="/search" element={<Search />} />
+                <Route path="/movie/:id" element={<MovieDetails />} />
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </div>
+          </BrowserRouter>
+        </FavoritesProvider>
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+};
 
 export default App;
